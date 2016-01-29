a870f2908d54a744194aadb34c3a0c049acc2933
Asaf <me@asafnudler.com>
2016-01-29 11:33:44 +0200

    Paintings gitignore

---
+++ core/src/com/retrom/ggj2016/LineSegment.java
@@ -0,0 +1,11 @@
+package com.retrom.ggj2016;
+
+/**
+ * Created by Asaf on 29/01/2016.
+ */
+public class LineSegment {
+    public float startX;
+    public float startY;
+    public float endX;
+    public float endY;
+}
